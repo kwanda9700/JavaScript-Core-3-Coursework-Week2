ee134ecfa74dfbe447a02fc09aaaada739f4d55a
Kwanda Silekwa <46996545+kwanda9700@users.noreply.github.com>
2022-10-11 07:47:05 +0200

    complete

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,21 @@
+const liElement = document.querySelector("li");
+const btnElement = document.getElementById("btn");
+
+btnElement.addEventListener("click", getImage);
+
+function getImage() {
+  liElement.innerHTML = "";
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return response.json();
+    })
+    .then((image) => {
+      const imageElement = document.createElement("img");
+      imageElement.src = image.message;
+      liElement.appendChild(imageElement);
+    })
+    .catch((error) => console.warn(error));
+}
